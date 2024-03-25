b033825fab296f77e711d40c2e0ded56da634769
Artur Matos <arturrtutu@gmail.com>
2024-03-25 13:52:45 -0300

    Convertendo classe primitiva para classe wrapped

---
+++ src/artur/matos/classeprimitivaewrapper.java
@@ -0,0 +1,14 @@
+package artur.matos;
+
+public class classeprimitivaewrapper {
+
+	public static void main(String[] args) {
+		int num1 = 9120;
+		System.out.println(num1);
+		Integer num2 = Integer.valueOf(num1);
+		System.out.println(num2);
+		
+
+	}
+
+}
